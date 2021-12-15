eb1a4fa3b7c805851f269557415c874ebaa4d42a
Jacobo <likenscoobo4@gmail.com>
2021-12-15 10:37:44 +0100

    rever_iterator

--- utils.hpp
+++ utils.hpp
@@ -57,9 +57,9 @@ namespace ft
 	struct iterator_traits
 	{
    		typedef typename Iterator::difference_type		difference_type;
-    	typedef typename Iterator::value_type 			value_type;
-    	typedef typename Iterator::pointer 				pointer;
-    	typedef typename Iterator::reference 			reference;
+    	typedef typename Iterator::value_type			value_type;
+    	typedef typename Iterator::pointer				pointer;
+    	typedef typename Iterator::reference			reference;
     	typedef typename Iterator::iterator_category	iterator_category;
 	};
 
