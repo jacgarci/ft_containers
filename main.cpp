4af8e31d1cae8a27cb4047a2654fec12a4a6e618
Jacobo <likenscoobo4@gmail.com>
2021-12-15 10:34:46 +0100

    rever_iterator

--- main.cpp
+++ main.cpp
@@ -2,14 +2,31 @@
 #include <vector>
 #include <type_traits>
 #include <iostream>
+#include <typeinfo>
 #include "utils.hpp"
 
 int main()
 {
-    ft::enable_if<0, char>::type k;
+    std::vector<int> a;
+    std::vector<int>::iterator it;
+    if (typeid(std::iterator_traits<std::vector<int>::iterator>::iterator_category)==typeid(std::random_access_iterator_tag))
+        std::cout << "int* is a random-access iterator";
+    /* std::cout << std::boolalpha;
+    std::cout << "is_integral:" << std::endl;
+    std::cout << "char: " << std::is_integral<char>::value << std::endl;
+    std::cout << "int: " << std::is_integral<int>::value << std::endl;
+    std::cout << "float: " << std::is_integral<float>::value << std::endl;
+    std::cout << std::boolalpha;
+    std::cout << "is_integral:" << std::endl;
+    std::cout << "char: " << ft::is_integral<char>::value << std::endl;
+    std::cout << "int: " << ft::is_integral<int>::value << std::endl;
+    std::cout << "float: " << ft::is_integral<float>::value << std::endl; */
 
-    k = 65;
-    std::cout <<  k << std::endl;
+    /* typedef ft::iterator_traits<ft::random_access_iterator_tag> traits;
+    if (typeid(traits::iterator_category)==typeid(ft::random_access_iterator_tag))
+        std::cout << "int* is a random-access iterator";
+    return 0; */
+   
     return (0);
 }
 
--- utils.hpp
+++ utils.hpp
@@ -3,13 +3,130 @@
 
 namespace ft
 {
-    //enable_if
+    // enable_if
     template< bool Cond, class T = void >
     struct enable_if {};
 
     template< class T >
     struct enable_if< true, T > {typedef T type;};
     
+    // is_integral
+    template < class T, T v >
+	struct integral_constant
+	{
+        static const    T  value = v;
+        typedef         T  value_type;
+        typedef integral_constant type;
+	};
+
+    template <class T, T v>
+    const T integral_constant< T, v >::value;
+
+    #define _BOOL_CONSTANT(b) integral_constant<bool,(b)>
+    typedef _BOOL_CONSTANT(true) true_type;
+    typedef _BOOL_CONSTANT(false) false_type;
+
+	template < class T> struct is_integral_type                     : public false_type {};
+	template <> struct is_integral_type < bool >					: public true_type {};
+	template <> struct is_integral_type < char >					: public true_type {};
+	template <> struct is_integral_type < char16_t >				: public true_type {};
+	template <> struct is_integral_type < char32_t >				: public true_type {};
+	template <> struct is_integral_type < wchar_t >					: public true_type {};
+	template <> struct is_integral_type < signed char >				: public true_type {};
+	template <> struct is_integral_type < short int >				: public true_type {};
+	template <> struct is_integral_type < int >						: public true_type {};
+	template <> struct is_integral_type < long int >				: public true_type {};
+	template <> struct is_integral_type < long long int >			: public true_type {};
+	template <> struct is_integral_type < unsigned char >			: public true_type {};
+	template <> struct is_integral_type < unsigned short int >		: public true_type {};
+	template <> struct is_integral_type < unsigned int >			: public true_type {};
+	template <> struct is_integral_type < unsigned long int >		: public true_type {};
+	template <> struct is_integral_type < unsigned long long int >	: public true_type {};
+
+	template <typename T>
+		struct is_integral : public is_integral_type<T> {};
+    
+    // iterator_traits
+	struct input_iterator_tag {};
+	struct output_iterator_tag {};
+	struct forward_iterator_tag       : public input_iterator_tag {};
+	struct bidirectional_iterator_tag : public forward_iterator_tag {};
+	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+	
+	template<class Iterator>
+	struct iterator_traits
+	{
+   		typedef typename Iterator::difference_type		difference_type;
+    	typedef typename Iterator::value_type 			value_type;
+    	typedef typename Iterator::pointer 				pointer;
+    	typedef typename Iterator::reference 			reference;
+    	typedef typename Iterator::iterator_category	iterator_category;
+	};
+
+	template<class T>
+	struct iterator_traits<T*>
+	{
+    	typedef ptrdiff_t difference_type;
+    	typedef T value_type;
+    	typedef T* pointer;
+    	typedef T& reference;
+    	typedef random_access_iterator_tag iterator_category;
+	};
+
+	template< class T>
+	struct iterator_traits < const T * >
+	{
+		typedef ptrdiff_t						difference_type;
+		typedef T							value_type;
+		typedef T const *					pointer;
+		typedef T const &					reference;
+		typedef std::random_access_iterator_tag	iterator_category;
+	};
+
+	// iterator template
+	template<class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
+	struct iterator
+	{
+    	typedef T         value_type;
+    	typedef Distance  difference_type;
+    	typedef Pointer   pointer;
+    	typedef Reference reference;
+    	typedef Category  iterator_category;
+	};
+
+	// reverse_iterator
+	template <class Iterator>
+	class reverse_iterator : public iterator<typename iterator_traits<Iterator>::iterator_category,
+                      typename iterator_traits<Iterator>::value_type,
+                      typename iterator_traits<Iterator>::difference_type,
+                      typename iterator_traits<Iterator>::pointer,
+                      typename iterator_traits<Iterator>::reference>
+	{
+		protected:
+    		Iterator current;
+		public:
+    		typedef Iterator                                            iterator_type;
+    		typedef typename iterator_traits<Iterator>::difference_type difference_type;
+    		typedef typename iterator_traits<Iterator>::reference       reference;
+    		typedef typename iterator_traits<Iterator>::pointer         pointer;
+
+    		reverse_iterator();
+    		explicit reverse_iterator(Iterator x);
+    		template <class U> reverse_iterator(const reverse_iterator<U>& u);
+    		template <class U> reverse_iterator& operator=(const reverse_iterator<U>& u);
+    		Iterator base() const;
+    		reference operator*() const;
+    		pointer   operator->() const;
+    		reverse_iterator& operator++();
+    		reverse_iterator  operator++(int);
+    		reverse_iterator& operator--();
+    		reverse_iterator  operator--(int);
+    		reverse_iterator  operator+ (difference_type n) const;
+    		reverse_iterator& operator+=(difference_type n);
+    		reverse_iterator  operator- (difference_type n) const;
+    		reverse_iterator& operator-=(difference_type n);
+    		reference         operator[](difference_type n) const;
+	};
 };
 
 #endif
